c464e2003ad88d9df44d239618903c3db4fecdae
amitsafi45 <safiamit45@gmail.com>
2022-12-01 23:42:56 +0545

    learn: Generic Function along with interface

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('i am here');
+interface lengthy{
+    length:number
+}
+
+function getCountAndDescribe<T extends lengthy>(element:T){
+    let text='NO Element'
+
+    if(element.length===1){
+        text="got 1 element"
+
+    }
+    if(element.length>1){
+        text="got"+element.length+"element"
+
+    }
+ return [element,text]   
+}
+
+const data=getCountAndDescribe([1,2,3,4])
+console.log(data);
+const data2=getCountAndDescribe(["amit","safi"])
+console.log(data2);
